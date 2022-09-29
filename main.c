d27084e255b5e9248531d0dfb477608ff794241b
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-09-29 18:02:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #define OFF 0;
 #define ON 1;
+
 struct pin{
         int ofst_reg_fsel;
         int ofst_pin_fsel;
         int ofst_pin_st_clr;    
 
-};
-
+}pin6={0,18,6};
 
 extern memory_map();
 extern GPIODirectionOut(struct pin *p);
@@ -15,13 +15,8 @@ extern GPIOTurn(struct pin *p,int value);
 
 int  main(){
 
-	struct pin pin5;
-	pin5.ofst_reg_fsel = 0;
-	pin5.ofst_pin_fsel = 18;
-	pin5.ofst_pin_st_clr= 6;
-
 	memory_map();
-	GPIODirectionOut(&pin5);
-	GPIOTurn(&pin5,OFF);	
+	GPIODirectionOut(&pin6);
+	GPIOTurn(&pin6,OFF);	
 }
 
