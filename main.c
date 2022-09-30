8d901df0bda8d9587b4f3a0363e53e2bbc81f4b0
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-09-30 17:22:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#define OFF 0;
-#define ON 1;
+#define OFF 0
+#define ON 1
 
 struct pin{
         int reg_func_ofst;
@@ -9,13 +9,27 @@ struct pin{
 
 };
 
+extern void nanoSleep(void);
 extern void memory_map(void);
 extern void GPIODirectionOut(int reg_offset, int pin_offset);
 extern void GPIOTurn(int pin_offset,int value);
 
+
+void delay(int millis){
+	for(int i=0;i<millis;i++){
+		nanoSleep();
+	}
+}
+
 int  main(){
 	memory_map();
 	GPIODirectionOut(0,18);
-	GPIOTurn(6,OFF);	
+	
+	for(int i=0;i<10;i++){
+		 GPIOTurn(6,ON);
+	         delay(1000);
+       		 GPIOTurn(6,OFF);
+       		 delay(1000);    
+		
+	}	
 }
-
