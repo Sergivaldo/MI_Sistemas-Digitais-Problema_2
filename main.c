f7ecb9b87c092cbd3da0b9f4a7a12c7518f1ae4f
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-10-07 08:54:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,23 @@
 #include<stdio.h>
-#include<stdint.h>
+#include<string.h>
 
 extern void memory_map(void);
 extern void lcd_init(void);
 extern void delay(void);
 extern void set_out(void);
 extern void clear_lcd(void);
-extern void write(uint8_t c);
+extern void write(int c);
 
-void counter(){
-   int num = 0x39;
-   for (int i =9;i<=0;i--){
-      write(num);
-      delay_millis(1000);
-      clear_lcd();
-      num--;
-   }
+void write_str(char c[]){
+	int len = strlen(c);
+	for(int i=0;i<len;i++){
+		write(c[i]);
+	}
 }
-
-void delay_millis(int millis){
-  for(int i =0; i<millis;i++){
-    delay();
-  }
-}
-
 
 void main(){
   memory_map();
   set_out();
   lcd_init();
- 
-  counter();
+  write_str(" Hello, World!");
 }
-
