4c11e019c84b4029dc2eb47b2f277c4a1fd8b16e
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-10-07 17:06:35 -0300

    Delete gpio.h

--- gpio.h
+++
@@ -1,9 +0,0 @@
-#ifndef gpio_h   
-#define gpio_h
-
-extern void nanoSleep(void);
-extern void memory_map(void);
-extern void GPIODirectionOut(uint8_t reg_offset, uint8_t pin_offset);
-extern void GPIOTurn(uint8_t pin_offset,uint8_t state);
-
-#endif
