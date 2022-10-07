625c145288ee639dc7d413bf892dcbe4a3190e5e
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-10-06 22:38:22 -0300

    Update lcd.s

--- lcd.s
+++ lcd.s
@@ -48,19 +48,19 @@
         GPIOTurn rs, #\rs_value
 
         MOV R0,#7
-        BL get_bit
+        get_bit
         GPIOTurn d7, R1
 
         MOV R0,#6
-        BL get_bit
+        get_bit
         GPIOTurn d6, R1
 
         MOV R0,#5
-        BL get_bit
+        get_bit
         GPIOTurn d5, R1
 
         MOV R0,#4
-        BL get_bit
+        get_bit
         GPIOTurn d4, R1
 
         enable
@@ -68,22 +68,22 @@
         GPIOTurn rs, #\rs_value
 
         MOV R0,#3
-        BL get_bit
+        get_bit
         GPIOTurn d7, R1
 
 
         MOV R0,#2
-        BL get_bit
+        get_bit
         GPIOTurn d6, R1
 
 
         MOV R0,#1
-        BL get_bit
+        get_bit
         GPIOTurn d5, R1
 
 
         MOV R0,#0
-        BL get_bit
+        get_bit
         GPIOTurn d4, R1
 
         enable
@@ -126,13 +126,13 @@ delay:
 	BX LR
 	
 @ branch que pega um bit do valor hexadecimal do caractere
-get_bit:
+.macro get_bit:
         MOV R2,#1 @ Move 1 para o registrador R0
         LSL R2,R0 @ Desloca para esquerda o valor em R2 para a posição do bit passado em R0
         AND R1, R6,R2 @ Realiza a operação lógica and para que seja pego apenas o bit desejado
         LSR R1,R0 @ Desloca para o bit menos significativo o bit da posição desejada
-
-        BX LR
+	.ltorg
+ .endm
 
 .data
 
