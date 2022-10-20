de1ac68b358733c5052b06a0e8d65de9f59bb7af
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-10-20 15:46:09 -0300

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -5,7 +5,7 @@ extern void memory_map(void);
 extern void lcd_init(void);
 extern void set_out(void);
 extern void clear_lcd(void);
-extern void write(int c);
+extern void write_char(unsigned char c);
 extern void set_cursor(int pos);
 extern void cgram_addr(int local);
 
@@ -18,6 +18,6 @@ extern void map_d4(int reg,int pin_offset, int pin_num);
 
 void lcd(int e,int rs, int d7,int d6,int d5, int d4);
 
-void print_lcd(char c[]){
+void print_lcd(unsigned char c[]){
 
 #endif
