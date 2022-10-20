9f3765d183fec3461268fbf39d4bdd71e7ffb73e
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-10-20 20:14:05 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <unistd.h>			//Used for UART
+#include <fcntl.h>			//Used for UART
+#include <termios.h>		//Used for UART
+#include <time.h>
+#include "lcd.h"
+
+int uart0_filestream = -1;
+
+void delay(int sec){
+    int millis = 1000*sec;
+
+    clock_t start_time = clock();
+    while (clock() < start_time + millis)
+    {}
+}
+
+void uart_configure(){
+    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (uart0_filestream == -1)
+    {
+        printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
+    }
+
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+}
+
+void uart_send_bytes(char command){
+    //----- TX BYTES -----
+    unsigned char tx_buffer[20];
+    unsigned char *p_tx_buffer;
+
+    p_tx_buffer = &tx_buffer[0];
+    *p_tx_buffer++ = command;
+
+    if (uart0_filestream != -1)
+    {
+        int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
+        if (count < 0)
+        {
+            printf("UART TX error\n");
+        }
+    }
+}
+
+unsigned char* uart_receive_bytes (){
+    
+    if (uart0_filestream != -1)
+    {
+        // Read up to 255 characters from the port if they are there
+        unsigned char rx_buffer[256];
+        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+        if (rx_length < 0)
+        {
+            //An error occured (will occur if there are no bytes)
+        }
+        else if (rx_length == 0)
+        {
+            //No data waiting
+        }
+        else
+        {
+            //Bytes received
+            rx_buffer[rx_length] = '\0';
+            return rx_buffer;
+        }
+    }
+    unsigned char *received_bytes = rx_buffer;
+    return received_bytes;
+}
+
+void main(){
+  uart_configure();
+  uart_send_bytes('H');
+  unsigned char* response = uart_receive_bytes();
+}
