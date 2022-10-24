9a2a30d72ab18759283c3a9bc0fad57d820c7f67
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-10-24 11:23:00 -0300

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -18,6 +18,6 @@ extern void map_d4(int reg,int pin_offset, int pin_num);
 
 void lcd(int e,int rs, int d7,int d6,int d5, int d4);
 
-void print_lcd(unsigned char c[]){
+void print_lcd(unsigned char c[]);
 
 #endif
