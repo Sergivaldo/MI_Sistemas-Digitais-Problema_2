d70a21720ffe1180f99f63b85434f87cde38bfb8
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-10-26 09:32:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <fcntl.h>			//Used for UART
 #include <termios.h>		//Used for UART
 #include <time.h>
-#include "lcd.h"
 
 // Comandos de requisição
 #define GET_NODEMCU_SITUATION 0x03
@@ -12,6 +11,60 @@
 #define SET_ON_NODEMCU_LED 0x06
 #define SET_OFF_NODEMCU_LED 0x07
 #define TRUE 1
+
+// Endereços
+
+#define NOT_ADDRESS 0
+
+extern void memory_map(void);
+extern void lcd_init(void);
+extern void set_out(void);
+extern void clear_lcd(void);
+extern void write_char(unsigned char c);
+extern void set_cursor(int pos);
+extern void cgram_addr(int local);
+
+extern void map_e(int reg,int pin_offset, int pin_num);
+extern void map_rs(int reg,int pin_offset, int pin_num);
+extern void map_d7(int reg,int pin_offset, int pin_num);
+extern void map_d6(int reg,int pin_offset, int pin_num);
+extern void map_d5(int reg,int pin_offset, int pin_num);
+extern void map_d4(int reg,int pin_offset, int pin_num);
+
+void lcd(int e,int rs, int d7,int d6,int d5, int d4){
+        memory_map();
+
+        int reg= 4 * (e/10);
+        int pin_offset = 3 *(e%10);
+        map_e(reg,pin_offset,e);
+
+        reg = 4 *(rs/10);
+        pin_offset = 3*(rs%10);
+        map_rs(reg,pin_offset,rs);
+
+        reg = 4 *(d7/10);
+        pin_offset = 3*(d7%10);
+        map_d7(reg,pin_offset,d7);
+
+        reg = 4 *(d6/10);
+        pin_offset = 3*(d6%10);
+        map_d6(reg,pin_offset,d6);
+
+        reg = 4 *(d5/10);
+        pin_offset = 3*(d5%10);
+        map_d5(reg,pin_offset,d5);
+
+        reg = 4 *(d4/10);
+        pin_offset = 3*(d4%10);
+        map_d4(reg,pin_offset,d4);
+
+        set_out();
+        lcd_init();
+}
+
+
+
+
 int uart0_filestream = -1;
 
 void delay(int sec){
@@ -84,39 +137,34 @@ unsigned char* uart_receive_bytes (){
 }
 
 void main(){
-	
-	lcd(1,25,21,20,16,12); 
-	uart_configure(); // Inicia as configurações do uart.
-	unsigned char input = 0;
+  lcd(1,25,21,20,16,12); 
+  uart_configure(); // Inicia as configurações do uart.
+  int input = 0;
 	while(TRUE){
-		printf("Envie um comando:");
-		printf("1 - Solicitar a situação do NODEMCU");
-		printf("2 - Solicitar o valor da entrada analógica");
-		printf("3 - Solicitar o valor de uma entrada digital");
-		printf("4 - Acender led da NODEMCU");
-		printf("5 - Apagar led do NODEMCU");
+		printf("Envie um comando:\n");
+		printf("1 - Solicitar a situação do NODEMCU\n");
+		printf("2 - Solicitar o valor da entrada analógica\n");
+		printf("3 - Solicitar o valor de uma entrada digital\n");
+		printf("4 - Acender led da NODEMCU\n");
+		printf("5 - Apagar led do NODEMCU\n");
 		printf("\n\n");
-		scanf("%c",&input);
-		
+		scanf("%d",&input);
 		switch(input){
 			case 1:
-				uart_send_bytes(GET_NODEMCU_SITUATION,0);
+				uart_send_bytes(GET_NODEMCU_SITUATION,NOT_ADDRESS);
 				break;
 			case 2:
-				uart_send_bytes(GET_ANALOG_INPUT_VALUE,0);
+				uart_send_bytes(GET_ANALOG_INPUT_VALUE,NOT_ADDRESS);
 				break;
 			case 3:
-				uart_send_bytes(GET_DIGITAL_INPUT_VALUE,0);
+				uart_send_bytes(GET_DIGITAL_INPUT_VALUE,NOT_ADDRESS);
 				break;
 			case 4:
-				uart_send_bytes(SET_ON_NODEMCU_LED,0);
+				uart_send_bytes(SET_ON_NODEMCU_LED,NOT_ADDRESS);
 				break;
 			case 5:
-				uart_send_bytes(SET_OFF_NODEMCU_LED,0);
-				break;
-			default:
-				printf("Comando inválido");
+				uart_send_bytes(SET_OFF_NODEMCU_LED,NOT_ADDRESS);
 				break;
 		}
 	}
-}
+}  
