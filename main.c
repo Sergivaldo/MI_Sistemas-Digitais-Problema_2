82e926d04c7c2f1459dabc7ff1b02430d5ae7395
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-10-28 10:53:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,16 @@
 #include <fcntl.h>			//Used for UART
 #include <termios.h>		//Used for UART
 #include <time.h>
-#include <sys/ioctl.h>
 
 // Comandos de requisição
 #define GET_NODEMCU_SITUATION 0x03
+  
 #define GET_ANALOG_INPUT_VALUE 0x04
 #define GET_DIGITAL_INPUT_VALUE 0x05
 #define SET_ON_NODEMCU_LED 0x06
 #define SET_OFF_NODEMCU_LED 0x07
 #define TRUE 1
-
 // Endereços
-
 #define NOT_ADDRESS 0
 
 extern void memory_map(void);
@@ -24,7 +22,6 @@ extern void clear_lcd(void);
 extern void write_char(unsigned char c);
 extern void set_cursor(int pos);
 extern void cgram_addr(int local);
-
 extern void map_e(int reg,int pin_offset, int pin_num);
 extern void map_rs(int reg,int pin_offset, int pin_num);
 extern void map_d7(int reg,int pin_offset, int pin_num);
@@ -34,74 +31,68 @@ extern void map_d4(int reg,int pin_offset, int pin_num);
 
 void lcd(int e,int rs, int d7,int d6,int d5, int d4){
         memory_map();
-
         int reg= 4 * (e/10);
         int pin_offset = 3 *(e%10);
         map_e(reg,pin_offset,e);
-
         reg = 4 *(rs/10);
         pin_offset = 3*(rs%10);
         map_rs(reg,pin_offset,rs);
-
         reg = 4 *(d7/10);
         pin_offset = 3*(d7%10);
         map_d7(reg,pin_offset,d7);
-
         reg = 4 *(d6/10);
         pin_offset = 3*(d6%10);
         map_d6(reg,pin_offset,d6);
-
         reg = 4 *(d5/10);
         pin_offset = 3*(d5%10);
         map_d5(reg,pin_offset,d5);
-
         reg = 4 *(d4/10);
         pin_offset = 3*(d4%10);
         map_d4(reg,pin_offset,d4);
-
         set_out();
         lcd_init();
 }
 
 
 
-
-int fd = -1;
+int uart0_filestream = -1;
 
 void delay(int sec){
-    int millis = 1000*sec;
-
+    int millis = sec * 1000;
     clock_t start_time = clock();
     while (clock() < start_time + millis)
     {}
 }
 
 void uart_configure(){
-    fd = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
-    if (fd == -1)
+    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (uart0_filestream == -1)
     {
         printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
     }
 
     struct termios options;
-    tcgetattr(fd, &options);
+    tcgetattr(uart0_filestream, &options);
     options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;
     options.c_iflag = IGNPAR;
     options.c_oflag = 0;
     options.c_lflag = 0;
-    tcflush(fd, TCIFLUSH);
-    tcsetattr(fd, TCSANOW, &options);
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
 }
 
 void uart_send_bytes(unsigned char command,unsigned char addr){
     //----- TX BYTES -----
-    unsigned char tx_buffer[2];
-    tx_buffer[0] = command;
-    tx_buffer[1] = addr;
-	
-    if (fd != -1)
+    unsigned char tx_buffer[20];
+    unsigned char *p_tx_buffer;
+
+    p_tx_buffer = &tx_buffer[0];
+    *p_tx_buffer++ = command;
+    *p_tx_buffer++ = addr;
+
+    if (uart0_filestream != -1)
     {
-        int count = write(fd, &tx_buffer[0], 2);
+        int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
         if (count < 0)
         {
             printf("UART TX error\n");
@@ -109,30 +100,29 @@ void uart_send_bytes(unsigned char command,unsigned char addr){
     }
 }
 
-unsigned char uart_receive_bytes (){
-    
-  unsigned char value;
-  
-  if (read (fd, &value, 1) != 1)
-    return -1 ;
+void uart_receive_bytes (){
 
-  return  value & 0xFF ;
-    
-}
+    if (uart0_filestream != -1)
+    {
+        // Read up to 255 characters from the port if they are there
+        static unsigned char rx_buffer[256];
+        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+        if (rx_length < 0)
+        {
+            //An error occured (will occur if there are no bytes)
+        }
+        else if (rx_length == 0)
+        {
+            //No data waiting
+        }
+        else
+        {
+            //Bytes received
+            rx_buffer[rx_length] = '\0';
+	    printf("%x",rx_buffer[0]);
+        }
+    }
 
-int serialDataAvail ()
-{
-  int result ;
-
-  if (ioctl (fd, FIONREAD, &result) == -1)
-    return -1 ;
-
-  return result ;
-}
-
-void serialFlush ()
-{
-  tcflush (fd, TCIOFLUSH) ;
 }
 
 void main(){
@@ -160,12 +150,12 @@ void main(){
 				break;
 			case 4:
 				uart_send_bytes(SET_ON_NODEMCU_LED,NOT_ADDRESS);
+				uart_receive_bytes();
 				break;
 			case 5:
 				uart_send_bytes(SET_OFF_NODEMCU_LED,NOT_ADDRESS);
+				uart_receive_bytes();
 				break;
 		}
-		
-		serialFlush();
 	}
 }  
