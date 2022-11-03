b77c99d4c35bef2404feee2badd91c8afd54b5b4
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-11-03 10:51:19 -0400

    Update lcd.s

--- lcd.s
+++ lcd.s
@@ -1,17 +1,6 @@
-.global lcd_init
-.global set_out
-.global write_char
+.global init_lcd
 .global clear_lcd
-.global delay
-.global cgram_addr
-.global set_cursor
-
-.global map_e
-.global map_rs
-.global map_d7
-.global map_d6
-.global map_d5
-.global map_d4
+.global write_char
 
 .include "gpio.s"
 
@@ -22,15 +11,6 @@
         SWI 0                
 .endm
 
-@ branch que pega um bit do valor hexadecimal do caractere
-.macro get_bit
-        MOV R2,#1 @ Move 1 para o registrador R0
-        LSL R2,R0 @ Desloca para esquerda o valor em R2 para a posição do bit passado em R0
-        AND R1, R6,R2 @ Realiza a operação lógica and para que seja pego apenas o bit desejado
-        LSR R1,R0 @ Desloca para o bit menos significativo o bit da posição desejada
-	
-	.ltorg
-.endm
 @ Da um pulso no pino enable o ligando e desligando.
 .macro enable
         GPIOTurn e, #0
@@ -44,7 +24,18 @@
         .ltorg
 .endm
 
-.macro fset_init
+@ Seta todos os pinos do lcd como saída.
+.macro setOut
+        GPIODirectionOut e
+        GPIODirectionOut rs
+        GPIODirectionOut d7
+        GPIODirectionOut d6
+        GPIODirectionOut d5
+        GPIODirectionOut d4
+.endm
+
+@ Macro para setar o modo de operação do display para 4 bits.
+.macro fset
 
         GPIOTurn rs,#0
 
@@ -60,10 +51,107 @@
         .ltorg
 .endm
 
-.macro command data,rs_value
-        MOV R6, \data
+@ Macro que define se o cursor será exibido e piscará e se o display será ligado 
+.macro display d,c,b
 
-        GPIOTurn rs, #\rs_value
+        GPIOTurn rs,#0
+
+        GPIOTurn d7,#0
+
+        GPIOTurn d6, #0
+
+        GPIOTurn d5,#0
+
+        GPIOTurn d4, #0
+
+        enable
+
+        GPIOTurn rs, #0
+
+        GPIOTurn d7,#1
+
+        GPIOTurn d6,#\d     @ O display está ligado quando D é 1 e desligado quando D é 0
+
+        GPIOTurn d5,#\c     @ O cursor é exibido quando C é 1 e não é exibido quando C é 0
+ 
+        GPIOTurn d4,#\b     @ Faz o cursor piscar se B for igual a 1, se não, o cursor fica parado
+
+        enable
+
+        .ltorg
+.endm
+
+@ Define o modo de entrada do display. Faz com que o cursor ou display desloque
+@ para esquerda ou direita quando um caractere é escrito
+.macro entrymode i_d, s
+
+        GPIOTurn rs, #0
+
+        GPIOTurn d7,#0
+
+        GPIOTurn d6,#0
+
+        GPIOTurn d5,#0
+
+        GPIOTurn d4,#0
+
+        enable
+
+        GPIOTurn rs,#0
+
+        GPIOTurn d7,#0
+
+        GPIOTurn d6,#1
+
+        GPIOTurn d5,#\i_d  @ O cursor se move para a direita e para a esquerda quando quando um caractere é escrito ou lido
+
+        GPIOTurn d4,#\s    @ O display se move para a direita ou para a esquerda quando S é 1. O display não muda se S for 0
+
+        enable
+
+        .ltorg
+.endm
+
+
+@ Macro para limpar o display LCD.
+
+.macro clear
+
+        GPIOTurn rs,#0
+
+        GPIOTurn d7,#0
+
+        GPIOTurn d6,#0
+
+        GPIOTurn d5,#0
+
+        GPIOTurn d4,#0
+
+        enable
+
+        GPIOTurn rs,#0
+
+        GPIOTurn d7,#0
+
+        GPIOTurn d6,#0
+
+        GPIOTurn d5,#0
+
+        GPIOTurn d4,#1
+
+        enable
+        .ltorg
+.endm
+
+
+@ Macro para escrever um caractere no display
+@
+@ Parâmetros
+@ hex - Hexadecimal do caractere a ser escrito
+.macro write hex
+        MOV R6, \hex
+
+        GPIOTurn rs, #1
 
         MOV R0,#7
         get_bit
@@ -83,7 +171,7 @@
 
         enable
 
-        GPIOTurn rs, #\rs_value
+        GPIOTurn rs, #1
 
         MOV R0,#3
         get_bit
@@ -108,216 +196,78 @@
         .ltorg
 .endm
 
-offset_led:
-	MOV R5,R0
-	MOV R6,R1
-	MOV R9,R2
+.macro  config_lcd
+        setOut 
+        
+        fset
+        fset
+        fset
+        fset
+		
+        display 1, 1, 1 
+        entrymode 0, 0 
+        
+.endm
 
-	LDR R4,=led
-	STR R5,[R4]
-	
-	LDR R4,=led
-	ADD R4,#4
-	STR R6,[R4]
+@ branch que pega um bit do valor hexadecimal do caractere
+.macro get_bit
+        MOV R2,#1 @ Move 1 para o registrador R0
+        LSL R2,R0 @ Desloca para esquerda o valor em R2 para a posição do bit passado em R0
+        AND R1, R6,R2 @ Realiza a operação lógica and para que seja pego apenas o bit desejado
+        LSR R1,R0 @ Desloca para o bit menos significativo o bit da posição desejada
 
-	LDR R4,=led
-	ADD R4,#8
-	STR R9,[R4]
+.endm
+
+init_lcd:
+	config_lcd
 	BX LR
 
-map_e:
-	MOV R5,R0
-        MOV R6,R1
-        MOV R9,R2
-
-        LDR R4,=e
-        STR R5,[R4]
-
-        LDR R4,=e
-        ADD R4,#4
-        STR R6,[R4]
-
-        LDR R4,=e
-        ADD R4,#8
-        STR R9,[R4]
-        BX LR
-
-map_rs:
-
-	MOV R5,R0
-        MOV R6,R1
-        MOV R9,R2
-
-        LDR R4,=rs
-        STR R5,[R4]
-
-        LDR R4,=rs
-        ADD R4,#4
-        STR R6,[R4]
-
-        LDR R4,=rs
-        ADD R4,#8
-        STR R9,[R4]
-        BX LR
-
-map_d7:
-	MOV R5,R0
-        MOV R6,R1
-        MOV R9,R2
-
-        LDR R4,=d7
-        STR R5,[R4]
-
-        LDR R4,=d7
-        ADD R4,#4
-        STR R6,[R4]
-
-        LDR R4,=d7
-        ADD R4,#8
-        STR R9,[R4]
-        BX LR
-
-map_d6:
-	MOV R5,R0
-        MOV R6,R1
-        MOV R9,R2
-
-        LDR R4,=d6
-        STR R5,[R4]
-
-        LDR R4,=d6
-        ADD R4,#4
-        STR R6,[R4]
-
-        LDR R4,=d6
-        ADD R4,#8
-        STR R9,[R4]
-        BX LR
-
-map_d5:
-	MOV R5,R0
-        MOV R6,R1
-        MOV R9,R2
-
-        LDR R4,=d5
-        STR R5,[R4]
-
-        LDR R4,=d5
-        ADD R4,#4
-        STR R6,[R4]
-
-        LDR R4,=d5
-        ADD R4,#8
-        STR R9,[R4]
-        BX LR
-
-map_d4:
-	MOV R5,R0
-        MOV R6,R1
-        MOV R9,R2
-
-        LDR R4,=d4
-        STR R5,[R4]
-
-        LDR R4,=d4
-        ADD R4,#4
-        STR R6,[R4]
-
-        LDR R4,=d4
-        ADD R4,#8
-        STR R9,[R4]
-        BX LR
-
-on_led:
-	GPIOTurn led,#0
-	BX LR
-
-off_led:
-	GPIOTurn led,#1
-	BX LR
-
-out_led:
-	GPIODirectionOut led
-	BX LR
-
-lcd_init:
-	command #0x01,0
-	fset_init
-	fset_init
-	fset_init
-	fset_init
-	command #0x0e,0
-	command #0x06,0
-	BX LR
-	
-set_cursor:
-	MOV R9,#0x80
-	MOV R10,R0
-	ORR R11,R9,R10
-	command R11,0
-	
-	BX LR 
-	
 write_char:
 	MOV R9,R0
-	command R9,1
-	
-	BX LR
-	
-cgram_addr:
-	MOV R6,R0
-	MOV R5,#0x40
-	ADD R5,R6
-	command R5,0
-	
+	write R9
 	BX LR
 	
 clear_lcd:
-	command #0x01,0
+	clear
 	BX LR
+	
 
-@ Seta todos os pinos do lcd como saída.
-set_out:
-        GPIODirectionOut e
-        GPIODirectionOut rs
-        GPIODirectionOut d7
-        GPIODirectionOut d6
-        GPIODirectionOut d5
-        GPIODirectionOut d4
-	
-	BX LR
-	
 .data
+
+time1s:
+	.word 1 @ Tempo em segundos
+	.word 000000000 @ Tempo em nanossegundos
+
+time500ms:
+	.word 0 @ Tempo em segundos
+	.word 500000000 @ Tempo em nanossegundos
 time1ms:
-	.word 0
-	.word 1000000
-led:
-	.word 0
-	.word 0
-	.word 0
+	.word 0 @ Tempo em segundos
+	.word 1000000 @ Tempo em nanossegundos
+
 @ Lcd
 
 rs:
-	.word 0 @ offset para selecionar o registrador de função
-	.word 0 @ offset do pino no registrador de função
-	.word 0 @ offset do pino no registrador de set e clear
+	.word 8 @ offset para selecionar o registrador de função
+	.word 15 @ offset do pino no registrador de função
+	.word 25 @ offset do pino no registrador de set e clear
 e:
 	.word 0 @ offset para selecionar o registrador de função
-	.word 0 @ offset do pino no registrador de função
-	.word 0 @ offset do pino no registrador de set e clear
+	.word 3 @ offset do pino no registrador de função
+	.word 1 @ offset do pino no registrador de set e clear
 d4: 
-	.word 0 @ offset para selecionar o registrador de função
-	.word 0 @ offset do pino no registrador de função
-	.word 0 @ offset do pino no registrador de set e clear
+	.word 4 @ offset para selecionar o registrador de função
+	.word 6 @ offset do pino no registrador de função
+	.word 12 @ offset do pino no registrador de set e clear
 d5:
-	.word 0 @ offset para selecionar o registrador de função
-	.word 0 @ offset do pino no registrador de função
-	.word 0 @ offset do pino no registrador de set e clear 
+	.word 4 @ offset para selecionar o registrador de função
+	.word 18 @ offset do pino no registrador de função
+	.word 16 @ offset do pino no registrador de set e clear 
 d6:
-	.word 0 @ offset para selecionar o registrador de função
+	.word 8 @ offset para selecionar o registrador de função
 	.word 0 @ offset do pino no registrador de função
-	.word 0 @ offset do pino no registrador de set e clear
+	.word 20 @ offset do pino no registrador de set e clear
 d7:
- 	.word 0 @ offset para selecionar o registrador de função
-	.word 0 @ offset do pino no registrador de função
-	.word 0 @ offset do pino no registrador de set e clear
+ 	.word 8 @ offset para selecionar o registrador de função
+	.word 3 @ offset do pino no registrador de função
+	.word 21 @ offset do pino no registrador de set e clear
