9ee746797678e106b4df76adedb748cbdc648671
Sergivaldo Junior <72475500+Sergivaldo@users.noreply.github.com>
2022-11-03 10:50:32 -0400

    Update main.c

--- main.c
+++ main.c
@@ -18,44 +18,19 @@
 #define NOT_ADDRESS 0
 
 extern void memory_map(void);
-extern void lcd_init(void);
-extern void set_out(void);
+extern void init_lcd(void);
 extern void clear_lcd(void);
 extern void write_char(unsigned char c);
-extern void map_e(int reg,int pin_offset, int pin_num);
-extern void map_rs(int reg,int pin_offset, int pin_num);
-extern void map_d7(int reg,int pin_offset, int pin_num);
-extern void map_d6(int reg,int pin_offset, int pin_num);
-extern void map_d5(int reg,int pin_offset, int pin_num);
-extern void map_d4(int reg,int pin_offset, int pin_num);
 
-void lcd(int e,int rs, int d7,int d6,int d5, int d4){
-        memory_map();
-        int reg= 4 * (e/10);
-        int pin_offset = 3 *(e%10);
-        map_e(reg,pin_offset,e);
-        reg = 4 *(rs/10);
-        pin_offset = 3*(rs%10);
-        map_rs(reg,pin_offset,rs);
-        reg = 4 *(d7/10);
-        pin_offset = 3*(d7%10);
-        map_d7(reg,pin_offset,d7);
-        reg = 4 *(d6/10);
-        pin_offset = 3*(d6%10);
-        map_d6(reg,pin_offset,d6);
-        reg = 4 *(d5/10);
-        pin_offset = 3*(d5%10);
-        map_d5(reg,pin_offset,d5);
-        reg = 4 *(d4/10);
-        pin_offset = 3*(d4%10);
-        map_d4(reg,pin_offset,d4);
-        set_out();
-        lcd_init();
+void lcd(){
+	memory_map();
+	init_lcd();
+	clear_lcd();
 }
-
 
 void print_lcd(unsigned char c[]){
         int len = strlen(c);
+        
         for(int i=0;i<len;i++){
                 write_char(c[i]);
         }
@@ -92,17 +67,17 @@ void serialPutchar (const unsigned char c)
   write (uart0_filestream, &c, 1) ;
 }
 
-unsigned char serialGetchar ()
+unsigned char serialGetchar()
 {
   uint8_t value ;
 
   if (read (uart0_filestream, &value, 1) != 1)
     return -1 ;
 
-  return ((unsigned char)value) & 0xFF ;
+  return ((int) value) & 0xFF;
 }
 
-void serialGetS(){
+void serialGetString(){
     static unsigned char rx_buffer[100];
     char response_command = -1;
     if (uart0_filestream != -1)
@@ -111,25 +86,27 @@ void serialGetS(){
 		int rx_length = read(uart0_filestream, (void*)rx_buffer, 100);		//Filestream, buffer to store in, number of bytes to read (max)
 		if (rx_length < 0)
 		{
-		    //An error occured (will occur if there are no bytes)
+		    printf("Ocorreu um erro.");
 		}
 		else if (rx_length == 0)
 		{
-		    //No data waiting
+		    printf("Não há bytes para serem lidos.");
 		}
 		else
 		{
 		    //Bytes received
 		    rx_buffer[rx_length] = '\0';
-		    printf("%s",rx_buffer);
+		    print_lcd(rx_buffer);
 		}
 	}
 }
 
 void main(){ 
+  lcd();
   uart_configure(); // Inicia as configurações do uart.
   int input = 0;
 	while(TRUE){
+		
 		printf("Envie um comando:\n");
 		printf("1 - Solicitar a situação do NODEMCU\n");
 		printf("2 - Solicitar o valor da entrada analógica\n");
@@ -145,22 +122,24 @@ void main(){
 			case 2:
 				serialPutchar(GET_ANALOG_INPUT_VALUE);
 				delay(2);
-				serialGetS();
+				clear_lcd();
+				print_lcd("Analógico: ");
+				serialGetString();
 				break;
 			case 3:
 				serialPutchar(GET_DIGITAL_INPUT_VALUE);
 				delay(2);
-				serialGetchar();
+			        printf("%d",serialGetchar());
 				break;
 			case 4:
 				serialPutchar(SET_ON_NODEMCU_LED);
-				delay(2);
-				serialGetchar();
+				clear_lcd();
+				print_lcd("Led Ligado");
 				break;
 			case 5:
 				serialPutchar(SET_OFF_NODEMCU_LED);
-				delay(2);
-				serialGetchar();
+				clear_lcd();
+				print_lcd("Led Desligado");
 				break;
 		}
      
